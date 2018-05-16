72295bc54030c66b6c0475d7097a6b8c536c4d47
zyplnu <429510037@qq.com>
2018-05-16 20:41:52 +0800

    haha

--- src/test/HelloGit.java
+++ src/test/HelloGit.java
@@ -3,7 +3,6 @@ package test;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("hello git!");
         System.out.println("hello git agin!");
     }
 }
